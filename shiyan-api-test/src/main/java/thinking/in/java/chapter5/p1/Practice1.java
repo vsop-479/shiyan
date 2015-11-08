b185355fbad285aa6e5051f2704779fb85f683cf
vsop-479 <vsop_479@163.com>
2015-11-08 11:15:30 +0800

    第五章习题p1-p7

---
+++ shiyan-api-test/src/main/java/thinking/in/java/chapter5/p1/Practice1.java
@@ -0,0 +1,11 @@
+package thinking.in.java.chapter5.p1;
+
+/**
+ * Created by zhouhui on 2015/11/8.
+ */
+public class Practice1 {
+    public static String s;
+    public static void main(String[] args){
+        System.out.println(s);
+    }
+}

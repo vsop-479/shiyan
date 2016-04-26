f3017c8362a6acd0ab62fe2f6288b66b551bbee4
zhouhui <zhouhui1@cofco.com>
2016-04-26 17:17:53 +0800

    java.lang.String创建

--- shiyan-api-test/src/main/java/shiyan/test/SomeTest.java
+++ shiyan-api-test/src/main/java/shiyan/test/SomeTest.java
@@ -9,17 +9,5 @@ import java.util.Map;
  */
 public class SomeTest {
     public static void main(String[] args){
-       String s = "html5|os|H5WEIXIN";
-        System.out.println("html5|os|H5WEIXIN".indexOf("weixin"));
-        System.out.println("html5|os|H5WEIXIN".indexOf("H5WEIXIN"));
-
-        Map<String, String> configMap = new HashMap<String, String>();
-        String[] oslist = "1:ios,2:android,3:cms,4:work,5:html5,6:weixin,7:H5WEIXIN".split(",");
-        for (String l : oslist) {
-            String[] o = l.split(":");
-            //序号， os
-            configMap.put(o[0], o[1]);
-        }
-        System.out.println(configMap.get("7"));
     }
 }

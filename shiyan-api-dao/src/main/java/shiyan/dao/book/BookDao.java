2a15784f05f4b7ef23423081ae08aa4ca2212f66
zhouhui <zhouhui1@cofco.com>
2016-05-04 18:19:09 +0800

    java.lang.String创建

--- shiyan-api-dao/src/main/java/shiyan/dao/book/BookDao.java
+++ shiyan-api-dao/src/main/java/shiyan/dao/book/BookDao.java
@@ -1,6 +1,7 @@
 package shiyan.dao.book;
 
 import org.springframework.stereotype.Repository;
+import shiyan.domain.Article;
 
 /**
  * Created by Administrator on 2015/9/15.
@@ -8,4 +9,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface BookDao {
     public void doDao();
+    public Article getArticle();
 }
--- shiyan-api-dao/src/main/java/shiyan/dao/book/impl/BookDaoImpl.java
+++ shiyan-api-dao/src/main/java/shiyan/dao/book/impl/BookDaoImpl.java
@@ -1,14 +1,21 @@
 package shiyan.dao.book.impl;
 
+import org.bson.codecs.configuration.CodecRegistries;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 import shiyan.dao.book.BookDao;
+import shiyan.domain.Article;
 
 /**
  * Created by Administrator on 2015/9/15.
  */
 @Repository("bookDao")
 public class BookDaoImpl implements BookDao {
+    //java mongo本身操作已经很简单，目前spring的整合反而过于复杂，不建议用
+      @Autowired
+     private MongoTemplate mongoTemplate;
 //    @Autowired
 //    private RedisUtils redisUtils;
 //
@@ -23,5 +30,10 @@ public class BookDaoImpl implements BookDao {
     @Override
     public void doDao() {
         System.out.println("BookDaoImpl");
+    }
+
+    @Override
+    public Article getArticle() {
+        return mongoTemplate.findOne(new Query(), Article.class);
     }
 }

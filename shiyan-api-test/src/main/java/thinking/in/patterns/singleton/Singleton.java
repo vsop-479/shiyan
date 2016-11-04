00f57c6a5cef994270b246057ee5a64317fc7208
zhouhui <zhouhui1@cofco.com>
2016-11-04 18:05:32 +0800

    exclusion spring

---
+++ shiyan-api-test/src/main/java/thinking/in/patterns/singleton/Singleton.java
@@ -0,0 +1,23 @@
+//package thinking.in.patterns.singleton;
+//
+///**
+// * Created by Administrator on 2016/11/4.
+// */
+//public final class Singleton {
+//    private static Singleton s = new Singleton(47);
+//    private int i;
+//    private Singleton(int x){
+//        i = x;
+//    }
+//    public static Singleton getReference(){
+//        return s;
+//    }
+//
+//    public int getValue(){
+//        return i;
+//    }
+//
+//    public void setValue(int x){
+//        i = x;
+//    }
+//}

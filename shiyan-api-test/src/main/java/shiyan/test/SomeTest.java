9968eb42b6b99c832d032d00ee02c76c034923d2
zhouhui <zhouhui1@cofco.com>
2016-12-08 11:40:00 +0800

    exclusion spring

--- shiyan-api-test/src/main/java/shiyan/test/SomeTest.java
+++ shiyan-api-test/src/main/java/shiyan/test/SomeTest.java
@@ -10,17 +10,49 @@ import java.util.UUID;
  */
 public class SomeTest {
     public static void main(String[] args){
-        String str1 = "hello";
-        String str2 = "he" + new String("llo");
-        System.err.println(str1 == str2);
-        System.err.println(str1.equals(str2));
-        System.err.println(Double.NaN );
+        sqrt();
+
+
+
+
+    }
+
+//    因数都是成对出现，a*a > (a+n)*(a-n) 所以判断N是否是质数，除到N的平方根即可。
+    static void sqrt(){
+        System.out.println(Math.sqrt(15));
+    }
+
+
+    static void cantInitialAbstractClass(){
+        //        不能实例化抽象类
+//        new A();
+    }
+    static void NaNFalse(){
         System.err.println((Double.NaN > Double.NaN) || (Double.NaN <= Double.NaN));
+    }
+
+    static void charAtLittleFasterThanStartWith(){
+        String s = "abc";
+        long l = System.currentTimeMillis();
+        for(int i = 0; i < 10000; i++){
+            System.out.print(s.startsWith("a"));
+        }
+        System.out.println();
+        System.out.println(System.currentTimeMillis() - l);
+
+        long l2 = System.currentTimeMillis();
+        for(int i = 0; i < 10000; i++){
+            System.out.print((s.charAt(0) == 'a'));
+        }
+        System.out.println();
+        System.out.println(System.currentTimeMillis() - l2);
     }
 }
 
-
-class A{
+/**
+ * abstract class 不能实例化。
+ */
+abstract class A{
     private A(){
 
     }

34ee7cf75b454a5d3d4727640ad4a2bd47e1a440
zhouhui <zhouhui1@cofco.com>
2017-01-13 17:25:44 +0800

    exclusion spring

---
+++ shiyan-api-test/src/main/java/shiyan/test/effective/item13/members/AI.java
@@ -0,0 +1,17 @@
+package shiyan.test.effective.item13.members;
+
+/**
+ * Created by Administrator on 2017/1/13.
+ */
+public interface AI {
+    void s();
+}
+
+
+class AImpl implements AI{
+
+    @Override
+    public void s() {
+
+    }
+}

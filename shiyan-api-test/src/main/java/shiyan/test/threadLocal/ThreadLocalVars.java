c1216faea94c0f1a5c5b4b14d41cacdd4bdeabbb
zhouhui <zhouhui1@cofco.com>
2017-01-17 11:50:09 +0800

    exclusion spring

--- shiyan-api-test/src/main/java/shiyan/test/threadLocal/ThreadLocalVars.java
+++ shiyan-api-test/src/main/java/shiyan/test/threadLocal/ThreadLocalVars.java
@@ -2,6 +2,8 @@ package shiyan.test.threadLocal;
 
 /**
  * Created by Administrator on 2016/6/30.
+ * 每一个线程都有一个ThreadLocalMap, 保存ThreadLocal对象到值得映射。
+ * oid，name，description将存在每个线程的ThreadLocalMap中。
  */
 public class ThreadLocalVars {
     private static final ThreadLocal<String> oid = new ThreadLocal<String>();

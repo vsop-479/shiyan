24290f59fa978917460f1cd7d8eb6aabae9c445f
zhouhui <zhouhui1@cofco.com>
2017-01-19 16:38:56 +0800

    exclusion spring

---
+++ shiyan-api-test/src/main/java/shiyan/test/inherit/F.java
@@ -0,0 +1,10 @@
+package shiyan.test.inherit;
+
+/**
+ * Created by Administrator on 2017/1/19.
+ */
+public class F {
+    public F(String d){};
+}
+
+
---
+++ shiyan-api-test/src/main/java/shiyan/test/inherit/S.java
@@ -0,0 +1,10 @@
+package shiyan.test.inherit;
+
+/**
+ * Created by Administrator on 2017/1/19.
+ */
+public class S extends F{
+    public S(String d){
+        super(d);
+    }
+}

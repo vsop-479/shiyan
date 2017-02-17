586187c7823c8ce91bc42be8a3faf3ea666bb55a
zhouhui <zhouhui1@cofco.com>
2017-02-17 16:52:01 +0800

    exclusion spring

---
+++ shiyan-api-test/src/main/java/shiyan/test/effective/item15/mutable/A.java
@@ -0,0 +1,29 @@
+package shiyan.test.effective.item15.mutable;
+
+import shiyan.test.annotation.Interceptor;
+
+/**
+ * Created by Administrator on 2017/2/14.
+ */
+public class A {
+}
+
+class B{
+
+
+      private int b;
+        }
+
+class C extends B{
+    public static void main(String[] args){
+        f();
+    }
+    static void f(){
+        Integer d = 2147483647;
+        Integer i = new Double(Math.pow(2, 31)).intValue();
+        System.out.println(d);
+        System.out.println(i);
+        System.out.println(Math.pow(2, 31));
+        System.out.println(Integer.MAX_VALUE);
+    }
+}
